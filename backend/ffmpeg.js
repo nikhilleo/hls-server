1e4030b9fdc26f79c0fe52ac596b19db8ede76c3
nikhilleo <ramrakhyani94@gmail.com>
2021-03-25 19:31:51 +0530

    Only Converting Code

---
+++ backend/ffmpeg.js
@@ -0,0 +1,19 @@
+// you can run this file on its own
+// node ffmpeg.js
+// this will convert whatever file you have in the "videos" folder
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+ffmpeg('videos/video.mp4', { timeout: 432000 }).addOptions([
+    '-profile:v baseline',
+    '-s 640x480',
+    '-level 3.0',
+    '-start_number 0',
+    '-hls_time 10',
+    '-hls_list_size 0',
+    '-f hls'
+]).output('videos/output.m3u8').on('end', () => {
+    console.log('end');
+}).run();
